8c8f025cfaf2eeeb120edb21495747a1866503f8
tumbleweed <tumbleWeed_06@proton.me>
2023-05-29 21:24:02 +0530

    dataObj and async func to get the data

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-/**async function getInfoAsync(city) {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=e339348a7d6b4e7884b161812231405&q=${city}`,{mode: 'cors'})
-    const json = await response.json()
-    console.log('Async',json)
-}
-getInfoAsync('New-york')**/
 
-async function getInfo(city) {
-        return new Promise((resolve) => {
-            resolve(response = fetch(`https://api.weatherapi.com/v1/current.json?key=e339348a7d6b4e7884b161812231405&q=${city}`,{mode: 'cors'}))
-    })
+
+let requierdDataObj = {
+    temp_c: 'temperature',
+    wind_speed:'wind speed',
+    local_time:'local time',
+    name:'name'
+    
 }
 
-
-function infoToJson(city) {
-    return new Promise (resolve => {
-        resolve(getInfo(city)
-            .then(response => response.json())
-            .then(responseJson => {json = responseJson; return {json}}))
-    })
-    /**await getInfo(city)
-        .then(response => response.json())
-        .then(responseJson => {json = responseJson; return json})**/
-    //await console.log(json)
+async function getInfoAsync(city) {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=e339348a7d6b4e7884b161812231405&q=${city}`,{mode: 'cors'});
+    const json = await response.json();
+    console.log(json)
+    requierdDataObj.temp_c = json.current.temp_c;
+    requierdDataObj.wind_speed = json.current.wind_kph;
+    requierdDataObj.local_time = json.location.localtime;
+    requierdDataObj.name = json.location.name;
+    console.log(requierdDataObj)
 }
 
-async function getData(city) {
-    let newCity = await infoToJson('london')
-    await console.log(newCity.json.current.temp_c)
-}
-
-getData('london')
-
+getInfoAsync('london')
